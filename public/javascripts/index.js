e0b02ef493f0e92285e1ca400bc893b8875b4476
Antara <66917980+meowzy1@users.noreply.github.com>
2020-12-02 11:20:05 -0600

    Add structure for comments

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,36 @@
-// window.addEventListener("load", (event)=>{
-//     console.log("hello from javascript!")
-// })
+window.addEventListener("DOMContentLoaded", (e) => {
 
-window.addEventListener("DOMContentLoaded", () => {
-    
-})
+    document.getElementById("like").addEventListener("click", (e) => {
+        //another function here - should be in another file?
+    });
+
+    //submit button for comments?
+    document.getElementById("submit").addEventListener("click", (e) => {
+        e.preventDefault();
+        let userComment = document.getElementById("user-comment").value;
+        comment(userComment); //comment function outside of evLis
+    });
+});
+
+//assuming class is comments
+function comment(string) { //must have string as param
+    document.querySelector(".comments").innerHTML = ""; //check class name
+
+    fetch("comments", { //class name here
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            comment: string,
+        }),
+    })
+        .then((res) => res.json())
+        .then((json) => {
+            json.comments.forEach((comment) => {
+                document.querySelector(
+                    ".comments" //make sure class name is same
+                )
+            });
+        });
+}
