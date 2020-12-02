1620b9d5d3bf33d346b6ee0b613ef7040aebcd81
Nick <nikhilmenon@comcast.net>
2020-12-02 12:48:29 -0600

    Error handling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const articleRouter = require('./routes/articles')
 const bcrypt =  require('bcrypt')
 
 const app = express();
@@ -39,6 +40,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/articles', articleRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/articles.js
+++ routes/articles.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const {Article, Comment, FollowingUser, Like, User } = require('../db/models');
 const { asyncHandler, csrfProtection} = require('./utils')
-
 const router = express.Router();
 
-
-router.post('/articles', 
+router.get('/', 
 csrfProtection, 
 asyncHandler (async (req, res) => {
-    
+    res.render('articles', {token: req.csrfToken(), article: {}})
+}))
+
+
+
+router.post('/', 
+csrfProtection, 
+asyncHandler (async (req, res) => {
+    const articleId = parseInt(req.params.id)
     await Article.create({
         title: req.body.title,
         body: req.body.body,
-        userId: req.body.userId
+        userId: req.body.userId,
+        token: req.csrfToken()
 
     })
-   res.redirect('/articles:id')
+   res.redirect('/:id')
 }))
+
+
+module.exports = {
+    router
+
+}
