35ebe4be8eb5d84298bfb4dde2b9fa666237948a
java_me <antaraj@bu.edu>
2020-12-02 17:36:46 -0600

    finish article

--- routes/articles.js
+++ routes/articles.js
@@ -6,7 +6,8 @@ const router = express.Router();
 router.get(
     '/:id',
     asyncHandler(async (req, res) => {
-        res.render('articles', { token: req.csrfToken(), article: {} });
+        const article = await Article.findByPk(req.params.id);
+        res.render('article-single', { article });
     })
 );
 
@@ -30,5 +31,17 @@ router.post(
         res.redirect(`/${newArticle.id}`);
     })
 );
+
+router.get('/:id/edit', csrfProtection, asyncHandler(async (req, res) => {
+    const article = await Article.findByPk(req.params.id);
+    res.render('article-edit', { token: req.csrfToken(), article });
+}))
+
+router.post('/:id/edit', csrfProtection, asyncHandler(async (req, res) => {
+    const { title, body } = req.body;
+    const article = await Article.findByPk(req.params.id);
+    await article.update({ title, body });
+    res.redirect(`/articles/${req.params.id}`);
+}))
 
 module.exports = router;
