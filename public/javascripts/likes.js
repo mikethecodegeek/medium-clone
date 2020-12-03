d31f80df7b18fe5f434713f770c1ad618aec8277
java_me <antaraj@bu.edu>
2020-12-03 11:45:46 -0600

    add like functionality

---
+++ public/javascripts/likes.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+
+    document.getElementById("like").addEventListener("click", (e) => {
+        //another function here - should be in another file?
+    });
+});
--- routes/articles.js
+++ routes/articles.js
@@ -11,21 +11,21 @@ router.get(
     asyncHandler(async (req, res) => {
         res.render('articles', { token: req.csrfToken(), article: {} });
     })
-    );
+);
 
-router.post("/new",blockRoute, csrfProtection, asyncHandler(async(req, res) => {
-    const {title,body,userId} = req.body;
-    const newArticle = await Article.create({body,title,userId});
+router.post("/new", blockRoute, csrfProtection, asyncHandler(async (req, res) => {
+    const { title, body, userId } = req.body;
+    const newArticle = await Article.create({ body, title, userId });
     res.redirect(`/articles/${newArticle.id}`)
-}));   
-    
+}));
+
 router.get(
     '/:id',
     asyncHandler(async (req, res) => {
         const article = await Article.findByPk(req.params.id);
         res.render('article-single', { article });
     })
-    );
+);
 router.post(
     '/',
     csrfProtection,
@@ -39,24 +39,40 @@ router.post(
     })
 );
 
-router.get('/:id/edit',blockRoute, csrfProtection, asyncHandler(async (req, res) => {
+router.get('/:id/edit', blockRoute, csrfProtection, asyncHandler(async (req, res) => {
     const article = await Article.findByPk(req.params.id);
-    if (req.session.auth.userId == article.userId){
+    if (req.session.auth.userId == article.userId) {
         res.render('article-edit', { token: req.csrfToken(), article });
     } else {
         res.send('Sorry you cannot edit this article')
     }
 }))
 
-router.post('/:id/edit',blockRoute, csrfProtection, asyncHandler(async (req, res) => {
+router.post('/:id/edit', blockRoute, csrfProtection, asyncHandler(async (req, res) => {
     const { title, body } = req.body;
     const article = await Article.findByPk(req.params.id);
-    if (req.session.auth.userId == article.userId){
+    if (req.session.auth.userId == article.userId) {
         await article.update({ title, body });
         res.redirect(`/articles/${req.params.id}`);
     } else {
         res.send('Sorry you cannot edit this article')
     }
 }))
+
+const articleInteractions = {
+    like: false,
+    likeCount: 0
+    //comment: [],
+};
+
+router.patch("/:id/like", (req, res) => {
+    articleInteractions.likeCount += 1;
+    res.json({ like: articleInteractions.likeCount });
+});
+
+router.patch("/:id/unlike", (req, res) => {
+    articleInteractions.likeCount -= 1;
+    res.json({ like: articleInteractions.likeCount });
+});
 
 module.exports = router;
