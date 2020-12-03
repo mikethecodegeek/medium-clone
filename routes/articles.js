cf1e3135537414bd90716648eb7f6fb2bea0b1b5
mike <mikethecodegeek@gmail.com>
2020-12-02 20:23:12 -0800

    backend block of article editing

--- routes/articles.js
+++ routes/articles.js
@@ -41,14 +41,22 @@ router.post(
 
 router.get('/:id/edit',blockRoute, csrfProtection, asyncHandler(async (req, res) => {
     const article = await Article.findByPk(req.params.id);
-    res.render('article-edit', { token: req.csrfToken(), article });
+    if (req.session.auth.userId == article.userId){
+        res.render('article-edit', { token: req.csrfToken(), article });
+    } else {
+        res.send('Sorry you cannot edit this article')
+    }
 }))
 
 router.post('/:id/edit',blockRoute, csrfProtection, asyncHandler(async (req, res) => {
     const { title, body } = req.body;
     const article = await Article.findByPk(req.params.id);
-    await article.update({ title, body });
-    res.redirect(`/articles/${req.params.id}`);
+    if (req.session.auth.userId == article.userId){
+        await article.update({ title, body });
+        res.redirect(`/articles/${req.params.id}`);
+    } else {
+        res.send('Sorry you cannot edit this article')
+    }
 }))
 
 module.exports = router;
